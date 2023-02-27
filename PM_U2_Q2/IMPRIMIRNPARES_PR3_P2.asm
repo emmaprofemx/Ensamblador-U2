b6f391cf7636d93c314593efa1e5aac1df908ee5
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-02-27 13:11:02 -0600

    Imprimir n pares

---
+++ PM_U2_Q2/IMPRIMIRNPARES_PR3_P2.asm
@@ -0,0 +1,54 @@
+TITLE NombrePrograma
+
+;DESCRPICIÓN 
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+;         Luna Sanchez Juan Pablo.
+;         Ruiz Garcia Emmanuel Alejandro.
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRPICIÓN
+
+INCLUDE Irvine32.inc  
+
+.data
+; Área de Declaración de Variables
+
+
+msg1_14 db "Ingresa un número: ",0 
+
+
+;CONSTANTES
+
+.code
+
+	main74 PROC
+	
+	;Lógica del Programa
+
+	; Solicitar al usuario un número
+	mov edx, offset msg1_14
+	call writestring
+	call crlf
+
+	call readint ; Almacenar el número ingresado por el usuario en eax
+
+	mov ebx , eax 
+
+	mov eax , 2
+	mov edx , 0 
+	call crlf
+
+	loopoo:
+	call writedec
+	call crlf
+	add eax , 2
+	inc edx
+	cmp edx , ebx
+	jl loopoo
+
+	exit	
+	main74 ENDP
+	
+	END main74
